ac814376404ef25e6206697b4c5d8cc2243b67d8
Mamdasan S <mhsaber1375@gmail.com>
2023-12-31 13:37:24 +0100

    Fix a mistype for `v_diff` variable name

--- im2dhistgo.go
+++ im2dhistgo.go
@@ -62,13 +62,14 @@ func Im2dhist(input_layer *image.Gray, w int) [65536]uint32 {
 					v_diff := int(v_2) - int(v_1)
 					if v_diff < 0 {v_diff *= -1}
 
-					v_diff = uint32(v_diff)
-					twodhist[index1] += v_diff + 1
+					v_diff_incremented = uint32(v_diff) + 1
+
+					twodhist[index1] += v_diff_incremented
 					if v_1 == v_2 {
 						continue
 					}
 					index2 := uint16(v_2) + uint16(v_1)*256
-					twodhist[index2] += v_diff + 1
+					twodhist[index2] += v_diff_incremented
 				}
 			}
 
