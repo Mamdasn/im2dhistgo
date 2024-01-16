624c0553f26ab52738c998c24fe4dadaf07c6929
Farhad Sabrian <mhsaber1375@gmail.com>
2024-01-16 01:45:38 +0100

    Add a main file to the package

---
+++ main/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/Mamdasn/im2dhistgo"
+
+)
+
+func main() {
+	if len(os.Args) < 1 {
+		fmt.Println("Usage: ./im2dhistgo <input>")
+		os.Exit(1)
+	}
+	imagename := os.Args[1]
+
+	twodhist := im2dhistgo.Im2dhist_file(imagename)
+
+	fmt.Println(twodhist)
+	fmt.Println("Done.")
+}
